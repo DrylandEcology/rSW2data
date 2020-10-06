bfc1534bb60d81f25a71045d0ff121aef3d9f9fc
Daniel Schlaepfer <daniel.schlaepfer@alumni.ethz.ch>
2020-10-06 17:23:40 -0400

    Move function `calc_BareSoilEvapCoefs()` from rSFSW2 to rSW2data
    - copy code and unit tests
    - update namespace and documentation objects
    
    - see corresponding rSFSW2 commit 4ee4d74c9906a8af7959e0670e5c13706d308254

--- R/soils_check.R
+++ R/soils_check.R
@@ -1,4 +1,29 @@
 
+
+
+#' NAs present but only in deepest soil layers
+#'
+#' Checks that NAs are present and that NAs occur only grouped together in the
+#' right-most columns per row (e.g., deepest soil layers if columns represent
+#' soil layers and rows represent sites).
+#'
+#' @param x A data.frame, matrix, or array with at least two dimensions.
+#'
+#' @return A logical vector of length equal to the first dimension of \code{x}
+#'   with \code{TRUE} if there are n[k] \code{NA}s in the k-th row and they
+#'   occupy the k rightmost columns.
+#'
+has_NAs_pooled_at_depth <- function(x) {
+  stopifnot(!is.null(dim(x)))
+
+  sapply(
+    apply(x, 1, function(dat) rle(is.na(dat))),
+    function(dat) length(dat$values) <= 2 && dat$values[length(dat$values)]
+  )
+}
+
+
+
 #' Check validity of a table with soil layer depths
 #'
 #' The function runs four checks: \itemize{
